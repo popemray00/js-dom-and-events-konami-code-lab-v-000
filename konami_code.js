b0619040957e247928a1391b8f24d951f3131232
Michael Pope <popemray1986@gmail.com>
2018-06-24 16:57:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,13 +2,23 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  let body = document.querySelector('script')
+  let index = 0
 
-  script.addEventListener('keydown', function(e) {
-    if (e) {
-      alert("Congrats")
-    }
-    }
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+
+ if (key === alphabet[index]) {
+   index++;
+
+   if (index === alphabet.length) {
+     alert("Hurray!");
+
+     index = 0;
+   }
+ } else {
+   index = 0;
+ }
+}
   })
 
 }
