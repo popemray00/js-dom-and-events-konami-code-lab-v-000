3e203f23903e738b41bd37ebde406750a1f33d1d
Michael Pope <popemray1986@gmail.com>
2018-06-24 16:53:50 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,13 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  let body = document.querySelector('script')
+
+  script.addEventListener('keydown', function(e) {
+    if (e) {
+      alert("Congrats")
+    }
+    }
+  })
 
 }
